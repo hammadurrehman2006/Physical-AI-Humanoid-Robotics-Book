f7d5e7396d081dff141cd24a01ffa3cf550bef2f
Muhammad Hammad ur Rehman <hammadurrehman7707@gmail.com>
2025-12-20 19:20:02 +0500

    fix(css): disable tailwind preflight to prevent conflict with docusaurus styles

--- book/src/plugins/tailwind-config.js
+++ book/src/plugins/tailwind-config.js
@@ -2,8 +2,13 @@ module.exports = function tailwindPlugin(context, options) {
   return {
     name: "tailwind-plugin",
     configurePostCss(postcssOptions) {
-      // Appending Tailwind instead of overwriting all plugins
+      // Add Tailwind
       postcssOptions.plugins.push(require("@tailwindcss/postcss"));
+      // Add preset-env to transpile Tailwind's modern output
+      postcssOptions.plugins.push(require("postcss-preset-env")({
+        autoprefixer: { flexbox: 'no-2009' },
+        stage: 3,
+      }));
       return postcssOptions;
     },
   };
